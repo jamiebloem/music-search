bcf5e1792d606d4d8b386f7979530fdfc02d27f5
sjaak1992 <syakirahalgadri@gmail.com>
2021-04-10 11:17:33 +0200

    toevoeging sjaak

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import axios from "axios";
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+
 
 function App() {
     const [query, setQuery] = useState('Coldplay');
+    const [artist, setArtist] = useState(null);
 
-  function fetchData(event) {
-    console.log('hallo', query)
-  }
+    useEffect(() => {
+        async function fetchData(event) {
+            // console.log('hallo', query)
+            const response = await axios.get(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=${query}`);
+            // console.log(response.data.artists[0])
+            console.log(response.data)
 
-  return (
-      <div>
-        <input value={query} onChange={event => setQuery(event.target.value)} type="search"/>
-        <button onClick={fetchData}>Search</button>
+            if (response.data.artists){
+                setArtist(response.data.artists[0]);
+            }
+
+        }
+        fetchData();
+    }, [query] //we luisteren hier na de verandering van query
+   );
+
+    //haalt data meteen op
 
 
-      </div>
-  );
+
+    async function fetchData(event) {
+        // console.log('hallo', query)
+        const response = await axios.get(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=${query}`);
+        // console.log(response.data.artists[0])
+        setArtist(response.data.artists[0]);
+
+    }
+
+    // console.log(artist)
+
+    return (
+        <>
+            <div>
+                <input value={query} onChange={event => setQuery(event.target.value)} type="search"/>
+                <button onClick={fetchData}>Search</button>
+            </div>
+            <div>
+                <h1>
+                    {artist && artist.strArtist}
+                </h1>
+                <p>
+                    {artist && artist.strBiographyEN}
+                </p>
+            </div>
+
+        </>
+    );
 }
 
 export default App;
